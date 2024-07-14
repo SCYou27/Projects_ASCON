49c5e458aedebbc0bd61ffe2d9c63a4c167df7d5
Shih-Chun You <scy27@cl.cam.ac.uk>
2024-07-14 19:09:23 +0100

    add download script

---
+++ project_M-Os/0005_attack/template_attack_KEY_S_TABLES/download.sh
@@ -0,0 +1,2 @@
+DIR='https://www.cl.cam.ac.uk/research/security/datasets/ascon/M-Os/0005_attack/template_attack_KEY_S_TABLES/'
+wget ${DIR}Tables.zip
---
+++ project_M-Os/0005_attack/template_attack_MASK_O_TABLES/download.sh
@@ -0,0 +1,2 @@
+DIR='https://www.cl.cam.ac.uk/research/security/datasets/ascon/M-Os/0005_attack/template_attack_MASK_O_TABLES/'
+wget ${DIR}Tables.zip
