912f3e885687bef2e72ce20ad60584551684bb54
Shih-Chun You <scy27@cl.cam.ac.uk>
2024-07-14 19:26:52 +0100

    add download script

---
+++ project_M-Os/0005_attack/data_fragments/download.sh
@@ -0,0 +1,3 @@
+DIR='https://www.cl.cam.ac.uk/research/security/datasets/ascon/M-Os/0005_attack/data_fragments/'
+wget ${DIR}key_bytes_O.zip
+wget ${DIR}key_bytes_S.zip
---
+++ project_U-Os/0005_attack/data_fragments/download.sh
@@ -0,0 +1,4 @@
+DIR='https://www.cl.cam.ac.uk/research/security/datasets/ascon/U-Os/0005_attack/data_fragments/'
+wget ${DIR}key_bits.zip
+wget ${DIR}key_bytes.zip
+wget ${DIR}key_16bits.zip
