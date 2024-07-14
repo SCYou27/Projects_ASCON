09e314087f814b37f105a915ba12eb4ed4bd495f
Shih-Chun You <scy27@cl.cam.ac.uk>
2024-07-14 19:07:31 +0100

    add download script

---
+++ project_U-Os/0005_attack/template_attack_16bits_O_TABLES/download.sh
@@ -0,0 +1,5 @@
+DIR='https://www.cl.cam.ac.uk/research/security/datasets/ascon/U-Os/0005_attack/template_attack_16bits_O_TABLES/'
+for i in $(seq -f "%02g" 1 10)
+  do
+    wget ${DIR}Tables_L${i}.zip
+  done
---
+++ project_U-Os/0005_attack/template_attack_loopy_D99_TABLES_L01/download.sh
@@ -0,0 +1,3 @@
+TAG='01'
+DIR='https://www.cl.cam.ac.uk/research/security/datasets/ascon/U-Os/0005_attack/template_attack_loopy_D99_TABLES_L'${TAG}'/'
+wget ${DIR}Tables.zip
