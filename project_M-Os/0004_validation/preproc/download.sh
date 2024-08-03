42c157637d243278eac29aaec19fc8c774e0c5e6
Shih-Chun You <scy27@cl.cam.ac.uk>
2024-08-03 17:44:56 +0100

    add download scripts

---
+++ project_M-Os/0004_validation/preproc/download.sh
@@ -0,0 +1,3 @@
+DIR='https://www.cl.cam.ac.uk/research/security/datasets/ascon/M-Os/0004_validation/preproc/'
+wget ${DIR}Corrcoef.zip
+wget ${DIR}report.txt
---
+++ project_M-Os/0004_validation/template_validation_MASK_O/download.sh
@@ -0,0 +1,3 @@
+DIR='https://www.cl.cam.ac.uk/research/security/datasets/ascon/M-Os/0004_validation/template_validation_MASK_O/'
+wget ${DIR}Rank_O004.zip
+wget ${DIR}Result_Tables.zip
