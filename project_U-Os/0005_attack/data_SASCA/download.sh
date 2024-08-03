088e5d5cad544642eaf4891558f57c2b018a6fc5
Shih-Chun You <scy27@cl.cam.ac.uk>
2024-08-03 20:12:16 +0100

    add download scripts

---
+++ project_U-Os/0005_attack/data_SASCA/download.sh
@@ -0,0 +1,5 @@
+DIR='https://www.cl.cam.ac.uk/research/security/datasets/ascon/U-Os/0005_attack/data_SASCA/'
+wget ${DIR}data_key.zip
+wget ${DIR}data_nonce.zip
+wget ${DIR}data_plaintext.zip
+wget ${DIR}data_ciphertag.zip
