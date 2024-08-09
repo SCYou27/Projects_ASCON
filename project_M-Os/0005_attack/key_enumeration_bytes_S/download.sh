569c5dfaa6a08f723da9967636b4be1543606dc9
Shih-Chun You <scy27@cl.cam.ac.uk>
2024-08-09 23:23:10 +0100

    add download scripts

---
+++ project_M-Os/0005_attack/key_enumeration_bytes_S/download.sh
@@ -0,0 +1,4 @@
+DIR='https://www.cl.cam.ac.uk/research/security/datasets/ascon/M-Os/0005_attack/key_enumeration_bytes_S/'
+wget ${DIR}Iterations.zip
+wget ${DIR}Predictions.zip
+wget ${DIR}report.txt
