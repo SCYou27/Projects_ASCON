bb374496e4fb37c5497c5fde986bb2ef7cdc6932
Shih-Chun You <scy27@cl.cam.ac.uk>
2024-08-29 18:31:05 +0100

    add shell scripts.

---
+++ key_rank_jl/download.sh
@@ -0,0 +1,14 @@
+DIR='https://www.cl.cam.ac.uk/research/security/datasets/ascon/key_rank_jl/'
+wget ${DIR}U-Os-Tables_16bits-result.npy
+wget ${DIR}U-Os-Tables_bits-result.npy
+wget ${DIR}U-Os-Tables_bytes-result.npy
+wget ${DIR}U-Os-Tables_direct-result.npy
+wget ${DIR}U-O3-Tables_16bits-result.npy
+wget ${DIR}U-O3-Tables_bits-result.npy
+wget ${DIR}U-O3-Tables_bytes-result.npy
+wget ${DIR}U-O3-Tables_direct-result.npy
+wget ${DIR}M-Os-Tables_bytes_O-result.npy
+wget ${DIR}M-Os-Tables_bytes_S-result.npy
+wget ${DIR}M-Os-Tables_direct_O-result.npy
+wget ${DIR}M-Os-Tables_direct_S-result.npy
+wget ${DIR}report.txt
